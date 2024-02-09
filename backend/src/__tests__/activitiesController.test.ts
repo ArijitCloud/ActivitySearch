67fb77464b28f1016f688fa8750d6e073b92ce85
Arijit <arjtmndl@live.com>
2024-02-09 22:28:00 +0100

    add more tests

---
+++ backend/src/__tests__/activitiesController.test.ts
@@ -0,0 +1,36 @@
+import request from "supertest";
+import { app } from "../app";
+
+describe("readActivities", () => {
+  it("should get activities 200 response", async () => {
+    const response = await request(app).get("/api/v1/activities");
+    expect(response.statusCode).toBe(200);
+  });
+  it("should get activity by id 200 response", async () => {
+    const response = await request(app).get("/api/v1/activities/1");
+    expect(response.statusCode).toBe(200);
+  });
+  it("should get activity by id 404 response", async () => {
+    const response = await request(app).get("/api/v1/activity");
+    expect(response.statusCode).toBe(404);
+  });
+
+  it("should return success message for all activities", async () => {
+    const response = await request(app).get("/api/v1/activities");
+    expect(response.body.success).toBe(true);
+  });
+  it("should return success message for single activity", async () => {
+    const response = await request(app).get("/api/v1/activities/2");
+    expect(response.body.success).toBe(true);
+  });
+
+  it("should return defined data", async () => {
+    const response = await request(app).get("/api/v1/activities");
+    expect(response.body.data).toBeDefined();
+  });
+
+  it("should return empty string for single activity that doesn't exist", async () => {
+    const response = await request(app).get("/api/v1/activities/100");
+    expect(response.body.data).toBe("");
+  });
+});
--- backend/src/__tests__/readActivities.test.ts
+++ backend/src/__tests__/readActivities.test.ts
@@ -1,9 +1,34 @@
-import request from "supertest";
-import { app } from "../app";
+import fs from "fs/promises";
+import readActivities from "../data/readActivities";
+jest.mock("fs/promises");
 
 describe("readActivities", () => {
-  it("should get activities", async () => {
-    const response = await request(app).get("/api/v1/activities");
-    expect(response.status).toBe(200);
+  //reset mocks before each test
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should return activities", async () => {
+    const mockActivities = `[
+      { "id": 1, "title": "Activity 1" },
+      { "id": 2, "title": "Activity 2" }
+    ]`;
+    (fs.readFile as jest.MockedFunction<typeof fs.readFile>).mockResolvedValue(
+      mockActivities
+    );
+
+    const activities = await readActivities();
+
+    expect(activities).toEqual(JSON.parse(mockActivities));
+  });
+
+  it("should return an empty array when there's an error", async () => {
+    (fs.readFile as jest.MockedFunction<typeof fs.readFile>).mockRejectedValue(
+      new Error("File read error")
+    );
+
+    const activities = await readActivities();
+
+    expect(activities).toEqual([]);
   });
 });
